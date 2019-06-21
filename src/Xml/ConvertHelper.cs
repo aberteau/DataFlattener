67ce4aeb7af3db05b0432366bd76d33b896ad142
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-06-22 01:02:29 +0200

    Adding Xml project

---
+++ src/Xml/ConvertHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace DataFlattener.Xml
+{
+    public class ConvertHelper
+    {
+        public static string ToXml(DataTable table)
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                table.WriteXml(sw);
+                string xmlString = sw.ToString();
+                return xmlString;
+            }
+        }
+    }
+}
---
+++ src/Xml/DataTableConverter.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using DataFlattener.Core;
+
+namespace DataFlattener.Xml
+{
+    public class DataTableConverter
+        : IDataTableConverter
+    {
+        public string ToString(DataTable table)
+        {
+            string xmlString = ConvertHelper.ToXml(table);
+            return xmlString;
+        }
+    }
+}
