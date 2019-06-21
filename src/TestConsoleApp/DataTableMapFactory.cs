c4f2ffe29ce864d6c6876408b7e670ff1eccfc0f
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-06-22 01:33:23 +0200

    Using different DataTableConverters in TestConsoleApp

---
+++ src/TestConsoleApp/DataTableMapFactory.cs
@@ -0,0 +1,24 @@
+﻿using DataFlattener.Core;
+
+namespace DataFlattener.TestConsoleApp
+{
+    public class DataTableMapFactory
+    {
+        public static DataTableMap CreateTestMap()
+        {
+            DataTableMap tableMapping = new DataTableMap();
+
+            tableMapping.Columns.Add(new DataColumnMap("id", "id"));
+            tableMapping.Columns.Add(new DataColumnMap("name", "name"));
+            tableMapping.Columns.Add(new DataColumnMap("range.start.page", "range_start_page"));
+            tableMapping.Columns.Add(new DataColumnMap("range.start.x", "range_start_x"));
+            tableMapping.Columns.Add(new DataColumnMap("range.start.y", "range_start_y"));
+
+            tableMapping.Columns.Add(new DataColumnMap("range.end.page", "range_end_page"));
+            tableMapping.Columns.Add(new DataColumnMap("range.end.x", "range_end_x"));
+            tableMapping.Columns.Add(new DataColumnMap("range.end.y", "range_end_y"));
+
+            return tableMapping;
+        }
+    }
+}
--- src/TestConsoleApp/Program.cs
+++ src/TestConsoleApp/Program.cs
@@ -1,9 +1,54 @@
-﻿namespace DataFlattener.TestConsoleApp
+﻿using System;
+using System.Data;
+using System.IO;
+using DataFlattener.Core;
+
+namespace DataFlattener.TestConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string p = Path.Combine(currentDirectory, @"..\..\..\..\DataSamples\Json\sample01.json");
+            String jsonFilePath = Path.GetFullPath(p);
+
+            string outputPath = @"F:\Temp\DataFlattener";
+            
+            string json = File.ReadAllText(jsonFilePath);
+
+            DataTableMap dataTableMap = DataTableMapFactory.CreateTestMap();
+
+            Json.DataMapper dataMapper = new Json.DataMapper();
+            DataTable dataTable = dataMapper.ToDataTable(json, dataTableMap);
+
+            Json.DataTableConverter dataTableJsonConverter = new Json.DataTableConverter();
+            string jsonOutput = dataTableJsonConverter.ToString(dataTable);
+            string jsonOutputFilePath = GetFilePath(outputPath, "json");
+            File.WriteAllText(jsonOutputFilePath, jsonOutput);
+
+            Xml.DataTableConverter dataTableXmlConverter = new Xml.DataTableConverter();
+            string xmlOutput = dataTableXmlConverter.ToString(dataTable);
+            string xmlOutputFilePath = GetFilePath(outputPath, "xml");
+            File.WriteAllText(xmlOutputFilePath, xmlOutput);
+
+            Csv.DataTableConverter dataTableCsvConverter = new Csv.DataTableConverter();
+            string csvOutput = dataTableCsvConverter.ToString(dataTable);
+            string csvOutputFilePath = GetFilePath(outputPath, "csv");
+            File.WriteAllText(csvOutputFilePath, csvOutput);
+
+            Console.WriteLine("End");
+            Console.WriteLine("Press [Enter] to exit...");
+            Console.ReadLine();
+        }
+
+        private static string GetFilePath(string jsonOutputPath, string extension)
+        {
+            string chrono = DateTime.Now.ToString("yyMMdd HHmmss");
+            string jsonFileName = $"{chrono}.{extension}";
+
+            string jsonPath = Path.Combine(jsonOutputPath, jsonFileName);
+            return jsonPath;
         }
     }
 }
