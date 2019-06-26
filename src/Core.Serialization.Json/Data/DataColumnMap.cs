7b33699a456a13ab20de8e49ee48227c8584a476
Amael BERTEAU <amael.berteau@tech-easy.fr>
2019-06-27 01:50:28 +0200

    Adding Core.Serialization.Json project

---
+++ src/Core.Serialization.Json/Data/DataColumnMap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace DataFlattener.Core.Serialization.Json.Data
+{
+    public class DataColumnMap
+    {
+        [JsonProperty("dataSourcePath")]
+        public String DataSourcePath { get; set; }
+
+        [JsonProperty("name")]
+        public String Name { get; set; }
+
+        [JsonProperty("dataType")]
+        public String DataType { get; set; }
+    }
+}
---
+++ src/Core.Serialization.Json/Serializer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace DataFlattener.Core.Serialization.Json
+{
+    public class Serializer
+    {
+        public static string Serialize(IEnumerable<DataColumnMap> dataColumnMaps)
+        {
+            IEnumerable<Data.DataColumnMap> dataColums = dataColumnMaps.Select(c => MapToData(c)).ToList();
+            string jsonString = JsonConvert.SerializeObject(dataColums, Formatting.Indented);
+            return jsonString;
+        }
+
+        public static DataColumnMap[] Deserialize(String json)
+        {
+            Data.DataColumnMap[] dataColums = JsonConvert.DeserializeObject<Data.DataColumnMap[]>(json);
+            DataColumnMap[]  columns = dataColums.Select(c => MapToCore(c)).ToArray();
+            return columns;
+        }
+
+        private static Data.DataColumnMap MapToData(DataColumnMap datacolumnMap)
+        {
+            Data.DataColumnMap dto = new Data.DataColumnMap();
+            dto.DataSourcePath = datacolumnMap.DataSourcePath;
+            dto.Name = datacolumnMap.Name;
+            dto.DataType = datacolumnMap.DataType.ToString();
+            return dto;
+        }
+
+        private static DataColumnMap MapToCore(Data.DataColumnMap datacolumnMap)
+        {
+            DataType dataType = MapToCore(datacolumnMap.DataType);
+            DataColumnMap map = new DataColumnMap(datacolumnMap.DataSourcePath, datacolumnMap.Name, dataType);
+            return map;
+        }
+
+        private static DataType MapToCore(string dataType)
+        {
+            return (DataType)Enum.Parse(typeof(DataType), dataType);
+        }
+    }
+}
